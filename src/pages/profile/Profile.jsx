f2914f7d8791bb6ac13bd45fa45ecb65011161f9
Sandra <sandrasuazo96@gmail.com>
2023-12-03 13:16:48 +0100

    Profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,3 +1,24 @@
+import { Box, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import { userData } from "../../core/userSlice";
+
 export const Profile = () => {
-  return <div>Perfil</div>;
+  const { token } = useSelector(userData);
+  const { user } = useSelector(userData);
+
+  return (
+    <Box>
+      {token ? (
+        <>
+          <Typography>Perfil:</Typography>
+          <Typography>{user.name}</Typography>
+          <Typography>{user.surname}</Typography>
+          <Typography>{user.email}</Typography>
+          <Typography>{user.phone}</Typography>
+        </>
+      ) : (
+        <Typography variant="h3">No user</Typography>
+      )}
+    </Box>
+  );
 };
